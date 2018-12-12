2a3aa4900b01df38ee4e2c62470c97712bd70f8a
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-12-12 08:13:50 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,26 @@ public class Player extends BareBones {
 
     @Override
     public void setHealth(int health) {
-        super.setHealth(health);
+        super.setHealth(health + 3);
     }
+
+    @Override
+    public void setIntelligence(int intelligence) {
+        super.setintelligence(intelligence + 3);
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        super.setSpeed(speed + 3);
+    }
+
+    @Override
+    public void setStrength(int strength) {
+        super.setStrength(strength + 3);
+    }
+
+
+
+
+
 }
